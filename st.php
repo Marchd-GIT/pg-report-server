0f3fee945f0c481ec176213faff82c6e5c3b70cf
Sergey Dyakov <admin@marchd.ru>
2016-09-18 18:13:18 -0400

    add export xls csv

--- st.php
+++ st.php
@@ -30,27 +30,25 @@ function get_select_row()
     if ($query !== '') {
         $json_params = json_decode($query);
         $datasets = get_datasets(true);
-        foreach( $datasets[$json_params->DataSet]->ParametersList as $val){
-           if ($val->id == $json_params->ID_Params)
-               $sql_query = $val->query;
+        foreach ($datasets[$json_params->DataSet]->ParametersList as $val) {
+            if ($val->id == $json_params->ID_Params)
+                $sql_query = $val->query;
         }
-        $empty=[];
-        query_run(get_connection_string($datasets[$json_params->DataSet]->DataStore),$empty, $sql_query);
-    }
-    else
+        $empty = [];
+        query_run(get_connection_string($datasets[$json_params->DataSet]->DataStore), $empty, $sql_query);
+    } else
         echo "Bed query!";
 }
 
 function get_connection_string($id_data_store)
 {
-    $path="settings/datastore.json";
+    $path = "settings/datastore.json";
     $file = fopen($path, "r");
     $json_text = fread($file, filesize($path));
-    //echo $json_text;
     $json = json_decode($json_text);
-    //echo $json[$id_data_store]->"connection_string";
-    //var_dump($json);
-    return $json[$id_data_store]->connection_string;
+    foreach ($json as $value)
+        if ($id_data_store == $value->id)
+            return $value->connection_string;
 }
 
 function get_datasets($flag)
@@ -64,26 +62,25 @@ function get_datasets($flag)
     }
     $datasets = array();
     foreach ($files as $val) {
-        if($flag)
-            array_push($datasets, json_params_get($val,"1"));
+        if ($flag)
+            array_push($datasets, json_params_get($val, "1"));
         else
-            array_push($datasets, json_params_get($val,"0"));
+            array_push($datasets, json_params_get($val, "0"));
     }
-    if($flag) {
+    if ($flag) {
         return $datasets;
-    }
-    else {
+    } else {
         echo json_encode($datasets, JSON_UNESCAPED_UNICODE);
     }
     return 0;
 }
 
-function json_params_get($dataset_file,$type)
+function json_params_get($dataset_file, $type)
 {
     $file = fopen("datasets/" . $dataset_file, "r");
     $json_text = fread($file, filesize("datasets/" . $dataset_file));
     $json = json_decode($json_text);
-    if($type == '0') {
+    if ($type == '0') {
         unset($json->SQL_Query);
         foreach ($json->ParametersList as $type)
             if ($type->type == "query")
@@ -92,7 +89,7 @@ function json_params_get($dataset_file,$type)
     return ($json);
 }
 
-function query_run($connection_string, $args_array, $query_string)
+function query_run($connection_string, $args_array, $query_string,$format)
 {
 
     $dbconn = pg_pconnect($connection_string);
@@ -108,42 +105,124 @@ function query_run($connection_string, $args_array, $query_string)
         exit;
     }
 
-    header('Content-Type: application/json');
 
-    $json_result =  new stdClass();
-    $json_result->fields = [];
-    $json_result->rows = [];
-    $i = 0;
-    while ($i < pg_num_fields($result)) {
-        $fieldName = pg_field_name($result, $i);
-        array_push($json_result->fields,$fieldName);
-        $i = $i + 1;
+    if($format == "json") {
+        header('Content-Type: application/json');
+
+        $json_result = new stdClass();
+        $json_result->fields = [];
+        $json_result->rows = [];
+        $i = 0;
+        while ($i < pg_num_fields($result)) {
+            $fieldName = pg_field_name($result, $i);
+            array_push($json_result->fields, $fieldName);
+            $i = $i + 1;
+        }
+        while ($row = pg_fetch_row($result))
+            array_push($json_result->rows, $row);
+
+        echo json_encode($json_result, JSON_UNESCAPED_UNICODE);
     }
-    while ($row = pg_fetch_row($result))
-           array_push($json_result->rows,$row);
+    elseif ($format == "xls") {
+        header('Content-Type: text/html; charset=utf-8');
+        header('P3P: CP="NOI ADM DEV PSAi COM NAV OUR OTRo STP IND DEM"');
+        header('Cache-Control: no-store, no-cache, must-revalidate');
+        header('Cache-Control: post-check=0, pre-check=0', FALSE);
+        header('Pragma: no-cache');
+        header('Content-transfer-encoding: binary');
+        header('Content-Disposition: attachment;');
+        header('Content-Type: application/x-unknown');
+        $i = 0;
+        echo '<html><body><table height=auto width=auto border=\'1\' rules=\'rows\' ><tr>';
+        while ($i < pg_num_fields($result)) {
+            $fieldName = pg_field_name($result, $i);
+            echo '<th bgcolor=\'#16a085\'>' . $fieldName . '</th>';
+            $i = $i + 1;
+        }
+        echo '</tr>';
+        $i = 0;
 
-    echo json_encode($json_result, JSON_UNESCAPED_UNICODE);
+        while ($row = pg_fetch_row($result)) {
+            echo '<tr>';
+            $count = count($row);
+            $y = 0;
+            while ($y < $count) {
+                $c_row = current($row);
+                echo '<td>' . $c_row . '</td>';
+                next($row);
+                $y = $y + 1;
+            }
+            echo '</tr>';
+            $i = $i + 1;
+        }
+
+    }
+    elseif ($format == "csv") {
+        header('Content-Type: text/html; charset=utf-8');
+        header('P3P: CP="NOI ADM DEV PSAi COM NAV OUR OTRo STP IND DEM"');
+        header('Cache-Control: no-store, no-cache, must-revalidate');
+        header('Cache-Control: post-check=0, pre-check=0', FALSE);
+        header('Pragma: no-cache');
+        header('Content-transfer-encoding: binary');
+        header('Content-Disposition: attachment;');
+        header('Content-Type: application/x-unknown');
+        $i = 0;
+        while ($i < pg_num_fields($result)) {
+            if($i>0)
+                echo "\t";
+            $fieldName = pg_field_name($result, $i);
+            echo $fieldName;
+            $i = $i + 1;
+        }
+        echo "\n";
+        $i = 0;
+
+        while ($row = pg_fetch_row($result)) {
+            $count = count($row);
+            $y = 0;
+            while ($y < $count) {
+                if ($y>0)
+                    echo "\t";
+                $c_row = current($row);
+                echo $c_row;
+                next($row);
+                $y = $y + 1;
+            }
+            echo "\n";
+            $i = $i + 1;
+        }
+
+    }
+
     pg_free_result($result);
     pg_close($dbconn);
 }
 
-function json_query_run()
+function json_query_run($format)
 {
     $query = isset($_POST['query']) ? $_POST['query'] : '';
     if ($query !== '') {
         $json_params = json_decode($query);
-        $datasets=get_datasets(true);
-        //debug
-        //echo get_connection_string($datasets[$json_params->DataSet]->DataStore); //constr
-        //var_dump($json_params->args); //params array
-       //echo $datasets[$json_params->DataSet]->SQL_Query; //sql query
+        //var_dump($json_params);
 
-        query_run(get_connection_string($datasets[$json_params->DataSet]->DataStore), $json_params->args, $datasets[$json_params->DataSet]->SQL_Query);
+        $datasets = get_datasets(true);
+
+        //var_dump($datasets);
+        //echo
+        //debug
+        //echo get_connection_string($datasets[$json_params->DataSet]->DataStore); //conection string
+        //var_dump($json_params->args); //params array
+        //echo $datasets[$json_params->DataSet]->SQL_Query; //sql query
+
+
+        query_run(get_connection_string($datasets[$json_params->DataSet]->DataStore), $json_params->args, $datasets[$json_params->DataSet]->SQL_Query,$format);
+
     } else {
         echo "Bed query!";
     }
 
 }
+
 
 $action = isset($_POST['action']) ? $_POST['action'] : '';
 switch ($action) {
@@ -151,7 +230,13 @@ switch ($action) {
         get_datasets(false);
         break;
     case "run_query":
-        json_query_run();
+        json_query_run("json");
+        break;
+    case "run_query_xls":
+        json_query_run("xls");
+        break;
+    case "run_query_csv":
+        json_query_run("csv");
         break;
     case "get_select_row":
         get_select_row();
@@ -159,7 +244,6 @@ switch ($action) {
     default:
         echo "Bed parameters!";
 }
-
 //debug
 //get_connection_string("1");
 //get_datasets();
