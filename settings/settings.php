e3066be5437f10e5b894dfda9d4b50a4ce88fa74
Sergey Dyakov <admin@marchd.ru>
2016-09-23 00:42:36 +0300

    add settings

---
+++ settings/settings.php
@@ -0,0 +1,11 @@
+<?php
+//*****HTTP Settings*****!!!
+//domain name
+$url='sr0.marchd.ru';
+//http or https
+$full_ulr="http://".$url;
+//Time Live Coockie
+$tlc=86400;
+//Slow query after in seconds
+$slow =0;
+?>
