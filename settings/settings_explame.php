f497142e06ed65cd872ccfbaab9ebe876059f133
Sergey Dyakov <admin@marchd.ru>
2016-09-26 06:08:10 +0300

    gitignore

---
+++ settings/settings_explame.php
@@ -0,0 +1,11 @@
+<?php
+//*****HTTP Settings*****!!!
+//domain name
+$url='domain.local';
+//http or https
+$full_ulr="http://".$url;
+//Time Live Coockie
+$tlc=86400;
+//Slow query after in seconds
+$slow = 5;
+?>
